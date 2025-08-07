d22a96d06f0a9e7e45659e2f2c1d225a63bf6bb2
hellohabib <habibdigitalmail@gmail.com>
2025-08-07 11:06:51 +0600

    Js module 18 - up to Task-3 done

--- 004-module-18-1.js
+++ 004-module-18-1.js
@@ -263,3 +263,140 @@ console.log(copyProducts);
 /* <<== এই কমেন্ট উঠাইয় দিয়ে প্রাক্টিস করতে হবে
 
 এই কমেন্ট উঠাইয় দিয়ে প্রাক্টিস করতে হবে ==>> */
+/*
+-------------------------------------------------
+** Challenge Tasks
+-------------------------------------------------
+*/
+/* 
+---------------------------------------------
+ ******** Task 1
+ ---------------------------------------------
+Write a JavaScript code to reverse the array colors without using the reverse method.
+
+Input: 
+const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+
+Output:
+['orange', 'yellow', 'green', 'blue', 'red']
+*/
+/* <<== এই কমেন্ট উঠাইয় দিয়ে প্রাক্টিস করতে হবে
+const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+console.log(colors);
+let reversedColors=[];
+for (color of colors){
+    reversedColors.unshift(color);
+}
+console.log(reversedColors);
+এই কমেন্ট উঠাইয় দিয়ে প্রাক্টিস করতে হবে ==>> */
+
+/*
+---------------------------------------------
+******** Task 2
+---------------------------------------------
+Write a JavaScript code to get the even numbers from an array using any looping technique.
+
+Input: 
+const numbers = [12, 98, 5, 41, 23, 78, 46];
+Output:
+[12, 98, 76, 46]
+*/
+/* <<== এই কমেন্ট উঠাইয় দিয়ে প্রাক্টিস করতে হবে
+const numbers = [12, 98, 5, 41, 23, 78, 46];
+let evenNumbers=[];
+for (number of numbers){
+    if(number%2==0){
+        evenNumbers.push(number);
+    }
+}
+console.log(evenNumbers);
+এই কমেন্ট উঠাইয় দিয়ে প্রাক্টিস করতে হবে ==>> */
+/*
+---------------------------------------------
+******** Task 3
+---------------------------------------------
+Use a for...of loop to concatenate all the elements of an array into a single string.
+Input: 
+var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+Output:
+'TomTimTinTik'
+*/
+/* <<== এই কমেন্ট উঠাইয় দিয়ে প্রাক্টিস করতে হবে
+var numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
+let result="";
+for(number of numbers){
+    result=result+number;
+}
+console.log(result);
+//*******output: TomTimTinTik
+এই কমেন্ট উঠাইয় দিয়ে প্রাক্টিস করতে হবে ==>> */
+/*
+---------------------------------------------
+******** Task 4 (Hard)
+---------------------------------------------
+Reverse the words of a sentence. Only the position of the word will be reversed. check out the output.
+Input: 
+const statement = 'I am a hard working person'
+Output:
+'person working hard a am I'
+*/
+/* <<== এই কমেন্ট উঠাইয় দিয়ে প্রাক্টিস করতে হবে
+
+//*******output: TomTimTinTik
+এই কমেন্ট উঠাইয় দিয়ে প্রাক্টিস করতে হবে ==>> */
+/*
+---------------------------------------------
+******** Task 5
+---------------------------------------------
+Copy the given array into another array so that changing the copy does not affect the original.Change the first element of the copied array to 99.
+
+Input: [1, 2, 3]
+
+Expected Output:
+
+Original: [1, 2, 3] Copy: [99, 2, 3]
+*/
+/* <<== এই কমেন্ট উঠাইয় দিয়ে প্রাক্টিস করতে হবে
+
+এই কমেন্ট উঠাইয় দিয়ে প্রাক্টিস করতে হবে ==>> */
+/*
+---------------------------------------------
+******** Task 6
+---------------------------------------------
+Given an array of student objects, print each student’s name and marks.
+
+[
+  { name: "John", marks: 85 },
+  { name: "Alice", marks: 90 }
+]
+Expected ooutput:
+John scored 85
+Alice scored 90
+*/
+/* <<== এই কমেন্ট উঠাইয় দিয়ে প্রাক্টিস করতে হবে
+
+এই কমেন্ট উঠাইয় দিয়ে প্রাক্টিস করতে হবে ==>> */
+/*
+---------------------------------------------
+******** Task 7
+---------------------------------------------
+Given a 2D array, update the value at second row first item to 99 and print the updated array.
+
+input:
+
+[
+  [1, 2],
+  [3, 4],
+  [5, 6]
+]
+Expected Array:
+
+[
+  [1, 2],
+  [99, 4],
+  [5, 6]
+]
+*/
+/* <<== এই কমেন্ট উঠাইয় দিয়ে প্রাক্টিস করতে হবে
+
+এই কমেন্ট উঠাইয় দিয়ে প্রাক্টিস করতে হবে ==>> */
